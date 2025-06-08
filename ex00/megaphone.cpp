5e35dc5d4e452376b5c2534f0239ee6b10589f88
hihi.louis <pmtriet1211@gmail.com>
2025-06-08 22:51:25 +0300

    add cmt

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,12 +6,13 @@
 /*   By: tripham <tripham@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/08 21:31:24 by tripham           #+#    #+#             */
-/*   Updated: 2025/06/08 22:38:57 by tripham          ###   ########.fr       */
+/*   Updated: 2025/06/08 22:49:18 by tripham          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <cstring>
+#include <cctype>
 
 int main(int argc, char **argv)
 {
@@ -21,10 +22,10 @@ int main(int argc, char **argv)
 	{
 		for (int i = 1; i < argc; i++)
 		{
-			std::string arg(argv[i]);
-			for (size_t j = 0; j < arg.length(); j++)
+			std::string arg(argv[i]);				  /*convert C-string to std::string to use .lenght() .at()*/
+			for (size_t j = 0; j < arg.length(); j++) /*arg.length return unsigned long, that's why j must be decleard by size_t*/
 			{
-				char up_case = static_cast<char>(std::toupper(arg.at(j)));
+				char up_case = static_cast<char>(std::toupper(arg.at(j))); /*std::toupper return int type, so it must be convert to char for correctly printing*/
 				std::cout << up_case;
 			}
 		}
