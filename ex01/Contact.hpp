72a32432facf93f0bfdd50beed646938af9d6097
Triet Pham <pmtriet1211@gmail.com>
2025-06-09 20:56:08 +0300

    startint ex01

---
+++ ex01/Contact.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Contact.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tripham <tripham@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/09 20:15:39 by tripham           #+#    #+#             */
+/*   Updated: 2025/06/09 20:55:46 by tripham          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CONTACT_CPP
+# define CONTACT_CPP
+
+#include <string>
+
+class contact{
+	private:
+		std::string firstName;
+		std::string lastName;
+		std::string nickName;
+		std::string phoneNumber;
+		std::string darkSecret;
+	public:
+		contact();
+		~contact();
+		
+		void setInfo();
+		void getTable(int i) const;
+		bool getFullInfo() const;
+};
+
+
+
+#endif
