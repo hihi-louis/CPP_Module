6dfb96455ec1fe0205da47d0450d6996e4c7053f
TRIET PHAM <106890020+PHAM-MINH-TRIET@users.noreply.github.com>
2025-06-21 18:40:59 +0200

    ex03 on the way

--- CPP_01/ex03/Weapon.cpp
+++ CPP_01/ex03/Weapon.cpp
@@ -6,7 +6,7 @@
 /*   By: tripham <tripham@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/20 13:10:53 by tripham           #+#    #+#             */
-/*   Updated: 2025/06/20 13:14:45 by tripham          ###   ########.fr       */
+/*   Updated: 2025/06/21 19:39:27 by tripham          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,3 +15,10 @@
 Weapon::Weapon(const std::string &newType): _type(newType){}
 Weapon::~Weapon() {}
 
+const std::string &Weapon::getType() const {
+    return (this->_type);
+}
+
+void Weapon::setType(const std::string newType) {
+    this->_type = newType;   
+}
