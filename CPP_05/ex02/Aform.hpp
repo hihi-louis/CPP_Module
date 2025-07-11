8961cd7a7ec9db4d2bb90302d082efecea49b5b1
Triet Pham <pmtriet1211@gmail.com>
2025-07-11 13:11:41 +0300

    rm .dist

--- CPP_05/ex02/Aform.hpp
+++ CPP_05/ex02/Aform.hpp
@@ -6,7 +6,7 @@
 /*   By: tripham <tripham@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/11 03:20:31 by tripham           #+#    #+#             */
-/*   Updated: 2025/07/11 03:21:49 by tripham          ###   ########.fr       */
+/*   Updated: 2025/07/11 13:09:52 by tripham          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,17 @@
 class Bureaucrat;
 
 class Aform{
-	
+	public:
+		Aform(std::string name, int gradeTosign, int gradeToExecute);
+		~Aform();
+		Aform(const Aform& other);
+		Aform& operator=(const Aform& other);
+		
+		std::string getName() const;
+		bool		getIssigned() const;
+		int			getGradeToSign() const;
+		int			getGradeToExecute() const;
+		bool 		execute(Bureaucrat const & executor) const;
+		
+		
 }
